a96741d927f523448faca5da1187847ef14d32cd
Dorian Marié <dorian@dorianmarie.com>
2024-08-24 01:05:13 +0200

    add argument parsing and specs for sorting csv

--- spec/sort_csv_spec.rb
+++ spec/sort_csv_spec.rb
@@ -1,6 +1,31 @@
 # frozen_string_literal: true
 
 require "spec_helper"
+require "tempfile"
 
 RSpec.describe "sort-csv" do
+  it "works" do
+    csv = Tempfile.create
+
+    File.write(csv, <<~INPUT)
+      id,name
+      2,Damien
+      1,Dorian
+      3,Laurie
+    INPUT
+
+    expect(`bin/sort-csv --headers #{csv.path} id`).to eq(<<~OUTPUT)
+      id,name
+      1,Dorian
+      2,Damien
+      3,Laurie
+    OUTPUT
+
+    expect(`bin/sort-csv --headers #{csv.path} --integer id`).to eq(<<~OUTPUT)
+      id,name
+      1,Dorian
+      2,Damien
+      3,Laurie
+    OUTPUT
+  end
 end
